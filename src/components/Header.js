ca7c5b805668d46164e55e00ee832bf84a69ad80
CamHung1609 <camdaica20003@gmail.com>
2024-11-14 10:30:26 +0700

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 function Item({ props }) {
   return (
     <>
-      <li>
-        <a href={props.link}>{props.content}</a>
+      <li className="nav-item">
+        <Link className="nav-link active" aria-current="page" to="{props.link}">
+          {props.content}
+        </Link>
       </li>
     </>
   );
@@ -13,41 +15,31 @@ function Item({ props }) {
 function Menu({ list }) {
   return (
     <>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Navbar
           </a>
-          <ul>
-        {list.map((duongDan, ind) => (
-          <Item key={ind} props={duongDan} />
-        ))}
-      </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              {list.map((duongDan, ind) => (
+                <Item key={ind} props={duongDan} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
